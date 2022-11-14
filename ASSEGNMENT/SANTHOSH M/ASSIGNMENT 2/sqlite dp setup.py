8f2b91ba5c6601a18baf2d61ed052b053fdc7250
SudhirS007 <114069563+sheikbeer@users.noreply.github.com>
2022-11-14 15:04:40 +0530

    folder structure

--- ASSEGNMENT/SANTHOSH M/ASSIGNMENT 2/sqlite dp setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('Users.db')
-print('opened database successfully')
-
-conn.execute('CREATE TABLE Users (name TEXT, email TEXT, mobile TEXT, city TEXT, state TEXT, country TEXT, password TEXT)')
-print("Table created successfully")
-conn.close()  
